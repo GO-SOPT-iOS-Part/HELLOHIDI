e6f54910a8852dcb8174320c73329b7d186f9085
HELLOHIDI <fbgmlwo123@naver.com>
2023-04-09 08:51:02 +0900

    Initial Commit

---
+++ GoSoptProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoSoptProject
+//
+//  Created by 류희재 on 2023/04/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
