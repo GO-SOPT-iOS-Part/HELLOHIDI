8a686d3a661039f0b74da223d1adaa245807679e
HELLOHIDI <fbgmlwo123@naver.com>
2023-04-11 16:26:50 +0900

    [Feat] welcomeView AutoLayout 설정 #3

--- GoSoptProject/SecondSeminarAssignment/Presentation/View/TvingWelcomeView.swift
+++ GoSoptProject/SecondSeminarAssignment/Presentation/View/TvingWelcomeView.swift
@@ -10,5 +10,42 @@ import UIKit
 import SnapKit
 import Then
 
-final class TvingWelcomeView: UIView {}
+final class TvingWelcomeView: UIView {
+    
+    private let adImage = UIImageView()
+    private let welcomeLabel = UILabel()
+    private lazy var goMainButton = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        style()
+        hierarchy()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func style() {
+//        adImage.do{}
+        
+        welcomeLabel.do {
+            $0.textColor = .tvingGray1
+            $0.textAlignment = .center
+        }
+    }
+    
+    private func hierarchy() {
+        self.addSubviews(adImage, welcomeLabel, goMainButton)
+    }
+    
+    private func layout() {
+        
+    }
+    
+    
+}
 
