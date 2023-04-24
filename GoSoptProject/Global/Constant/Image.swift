67231b847e13054b1a9643edad8a6d7f32ca54b0
HELLOHIDI <fbgmlwo123@naver.com>
2023-04-24 13:57:17 +0900

    [Add] 이미지 Asset 추가 #9

--- GoSoptProject/Global/Constant/Image.swift
+++ GoSoptProject/Global/Constant/Image.swift
@@ -11,9 +11,16 @@ public enum Image {
     
     //MARK: - TVING
     
+    //MARK: - Onbaording
+    
     static let eye = UIImage(named: "tving_eye")!
     static let close = UIImage(named: "tving_close")!
     static let back = UIImage(named: "tving_back")!
     static let adImage = UIImage(named: "tving_adImage")!
     
+    //MARK: - MyPage
+    static let alarm = UIImage(named: "tving_alarm")!
+    static let setting = UIImage(named: "tving_setting")!
+    static let cash = UIImage(named: "tving_cash")!
+    static let ticket = UIImage(named: "tving_ticket")!
 }
